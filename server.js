cd613cdd153ebbcb8e9620caba48780e2bdfc22c
shababtoha <shabab.toha@gmail.com>
2019-06-05 23:20:39 +0600

    one to one call

---
+++ server.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var app = express();
+
+var users = {};
+var idToName = {};
+
+var channel = {};
+
+const  port = process.env.PORT || 8080;
+
+var server = app.listen(port,  function() {
+    var port = server.address().port;
+    console.log('listening to port ' + port);
+});
+
+var io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+
+    //when new connection arrives send all online users
+    io.to(socket.id).emit('newconnection', users);
+
+    ///handle disconnect
+    socket.on('disconnect', function() {
+        var userName = idToName[socket.id];
+        delete idToName[socket.id];
+        delete users[userName];
+        io.emit('userDisconnected', userName);
+        console.log(userName + "disconnected");
+
+    });
+
+    ///handle create room
+    socket.on('create', function (newUser) {
+        console.log("Request User Creation, username " + newUser);
+        if(users.hasOwnProperty(newUser)) {
+            console.log("can not create user. already exist");
+            return;
+        }
+        users[newUser] = {
+            busy : false,
+            socketId : socket.id
+        };
+        idToName[socket.id] = newUser;
+        console.log("new user created. username", users[newUser]);
+        io.emit('newUser', newUser);
+    });
+
+    socket.on('message', function (message) {
+        //console.log(message);
+       // console.log()
+        io.to(channel[socket.id]).emit('message', message);
+    });
+
+    socket.on("initiateCall" , function (userName) {
+        var userSocketId = users[userName].socketId;
+        var intiator = idToName[socket.id];
+        users[userName].busy = true;
+        users[intiator].busy = true;
+        io.to(userSocketId).emit('recievedCall', intiator);
+        console.log(idToName[socket.id] + " is calling ", userName);
+        channel[socket.id] = userSocketId;
+        channel[userSocketId] = socket.id;
+    });
+
+    socket.on('recieved' , function (isRecieved) {
+        var recievedUserName = idToName[socket.id];
+        var initiatorUserName = idToName[channel[socket.id]];
+        if(isRecieved){
+            console.log(recievedUserName + " recieved ", initiatorUserName+"'s call");
+        }
+        else {
+            var recievedId = socket.id;
+            var initiatorId = channel[recievedId];
+            delete channel[recievedId];
+            delete channel[initiatorId];
+            users[recievedUserName].busy = false;
+            users[initiatorUserName].busy = false;
+        }
+        io.to(channel[socket.id]).emit("answer", isRecieved);
+
+    });
+});
+
+app.use(express.static('static'));
+
+app.get('/' , (req,res) => {
+    res.sendFile(process.cwd()+'/static/index.html');
+}) ;
---
+++ static/webrtc.js
@@ -0,0 +1,215 @@
+var socket = io();
+var peerConnection;
+let isChannelReady;
+let myName;
+
+//webrtc
+
+let initiator;
+
+const mediaStreamConstraints = {
+    video: true,
+    audio : true,
+};
+
+let iceServers;
+// iceServers.push({
+//     urls: [ "stun:ss-turn1.xirsys.com" ]
+// });
+
+iceServers = {
+    username: "7DYczzEyXySq7SPAcEEAPzMRhrg_UFZ5SpDYDgblZ9zfgIaWlOn03h7LZqU4f0TUAAAAAFz1Dh1zaGFiYWJ0b2hh",
+    credential: "85f1ac72-85f8-11e9-88a9-7a7a3a22eac8",
+    urls: [
+        "turn:ss-turn1.xirsys.com:80?transport=udp",
+        "turn:ss-turn1.xirsys.com:3478?transport=udp",
+        "turn:ss-turn1.xirsys.com:80?transport=tcp",
+        "turn:ss-turn1.xirsys.com:3478?transport=tcp",
+        "turns:ss-turn1.xirsys.com:443?transport=tcp",
+        "turns:ss-turn1.xirsys.com:5349?transport=tcp"
+    ]
+};
+
+const localVideo = document.getElementById('localVideo');
+console.log(localVideo);
+const remoteVideo = document.getElementById('remoteVideo');
+let localStream;
+let remoteStream;
+
+function gotLocalMediaStream(mediaStream) {
+    localStream = mediaStream;
+    localVideo.srcObject = mediaStream;
+    if(initiator) maybeStart();
+}
+
+function handleLocalMediaStreamError(error) {
+    console.log('navigator.getUserMedia error: ', error);
+}
+
+
+function getLocalMedia() {
+    navigator.mediaDevices.getUserMedia(mediaStreamConstraints)
+        .then(gotLocalMediaStream)
+        .catch(handleLocalMediaStreamError);
+}
+
+
+var createButton = document.getElementById('create');
+createButton.addEventListener('click', function () {
+    myName = document.getElementById("roomName").value;
+    socket.emit('create', myName);
+    createButton.disabled = true;
+});
+
+socket.on('newconnection', function (users) {
+    for(var userName in users) {
+        $("#userList").append('<li id="'+userName+'" > <p style="display: inline-block">'+userName +'</p> <button style="display: inline-block" onclick=startCall("'+userName+'")> call  </button></li>');
+    }
+});
+
+socket.on("newUser", function (userName) {
+    $("#userList").append('<li id="'+userName+'" ><p style="display: inline-block">'+userName +'</p>  <button style="display: inline-block" onclick=startCall("'+userName+'")> call </button> </li>');
+});
+
+socket.on("userDisconnected", function (userName) {
+    $("#"+userName).remove();
+});
+
+socket.on("recievedCall", function (userName) {
+    //todo-> answer or reject?
+    var answer = window.confirm(userName + " is calling you");
+    console.log(answer);
+    if(answer)getLocalMedia();
+    answerCall(answer);;
+});
+
+socket.on("answer", function (hasAnswered) {
+    if(hasAnswered) {
+        getLocalMedia();
+    }
+    else {
+        alert("Call rejected");
+    }
+});
+
+
+
+socket.on('message', function (message) {
+    console.log("Client Recieved A message", message) ;
+    switch(message.type) {
+        case 'offer' :
+            if(!initiator) {
+               maybeStart();
+            }
+            peerConnection.setRemoteDescription(new RTCSessionDescription(message));
+            doAnswer();
+            break;
+        case 'answer':
+            peerConnection.setRemoteDescription(new RTCSessionDescription(message));
+            break;
+        case 'candidate' :
+            var candidate = new RTCIceCandidate({
+                sdpMLineIndex: message.label,
+                candidate: message.candidate
+            });
+            peerConnection.addIceCandidate(candidate);
+            break;
+        default:
+            console.log("unwanted message", message);
+            break;
+    }
+
+});
+
+
+function doAnswer() {
+    console.log("sending answer to peer");
+    peerConnection.createAnswer()
+        .then(setLocalAndSendMessage,onCreateSessionDescriptionError );
+}
+
+function onCreateSessionDescriptionError(error) {
+    console.log('Failed to create session description: ' + error.toString());
+}
+
+function answerCall(answer) {
+    socket.emit('recieved',answer);
+}
+
+function sendMessage(message) {
+    console.log("sending message", message);
+    socket.emit('message', message);
+}
+
+const handleIceCandidate = function (event) {
+    console.log("iceCandidate :" , event);
+    if(event.candidate) {
+        sendMessage({
+            type: 'candidate',
+            label: event.candidate.sdpMLineIndex,
+            id: event.candidate.sdpMid,
+            candidate: event.candidate.candidate
+        })
+    } else {
+        console.log('End of candidates.');
+    }
+};
+
+function handleRemoteStreamAdded(event) {
+    console.log("remote stream added");
+    remoteStream = event.stream;
+    remoteVideo.srcObject = remoteStream;
+}
+
+function handleRemoteStreamRemoved(event) {
+    console.log('Remote stream removed. Event: ', event);
+}
+
+function createPeerConnection() {
+    try {
+        peerConnection = new RTCPeerConnection(iceServers);
+        peerConnection.onicecandidate = handleIceCandidate;
+        peerConnection.onaddstream = handleRemoteStreamAdded;
+        peerConnection.onremovestream = handleRemoteStreamRemoved;
+        console.log('Created RTCPeerConnnection');
+    } catch (e) {
+        console.log('Failed to create PeerConnection, exception: ' + e.message);
+        alert('Cannot create RTCPeerConnection object.');
+        return;
+    }
+}
+
+
+function doCall() {
+    console.log("sending offer to peer");
+    peerConnection.createOffer(setLocalAndSendMessage, handleCreateOfferError);
+}
+
+function handleCreateOfferError(event) {
+    console.log('createOffer() error: ', event);
+}
+
+function setLocalAndSendMessage(sessionDescription) {
+    peerConnection.setLocalDescription(sessionDescription);
+    console.log('setLocalAndSendMessage sending message', sessionDescription);
+    sendMessage(sessionDescription);
+}
+
+function maybeStart() {
+    console.log("startting ->>");
+    if(typeof localStream !==undefined) {
+        createPeerConnection();
+        peerConnection.addStream(localStream);
+        if(initiator)  doCall();
+    }
+}
+
+function startCall(name) {
+    if(name === myName) {
+        alert("trying to call yourself? are you DUMBASS?");
+        return;
+    }
+    initiator = true;
+    socket.emit('initiateCall', name);
+}
+
